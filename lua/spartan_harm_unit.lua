f8f26281f5ae0181a28c9de9574664748e590ad9
ForestDragon <kolya.vlasenko@gmail.com>
2023-03-03 11:46:05 +0200

    finally fixed phoenix amulet not working against ranged attacks or bombs

---
+++ lua/spartan_harm_unit.lua
@@ -0,0 +1,208 @@
+--based on the mainline [harm_unit] tag lua code, but edited so it works properly with phoenix amulet
+--(renamed tag, and calls [spartan_kill] tag)
+
+local helper = wesnoth.require "helper"
+local utils = wesnoth.require "wml-utils"
+local wml_actions = wesnoth.wml_actions
+local T = wml.tag
+
+function wml_actions.spartan_harm_unit(cfg)
+	local filter = wml.get_child(cfg, "filter") or wml.error("[harm_unit] missing required [filter] tag")
+	-- we need to use shallow_literal field, to avoid raising an error if $this_unit (not yet assigned) is used
+	if not wml.shallow_literal(cfg).amount then wml.error("[harm_unit] has missing required amount= attribute") end
+	local variable = cfg.variable -- kept out of the way to avoid problems
+	local _ = wesnoth.textdomain "wesnoth"
+	-- #textdomain wesnoth
+	local harmer
+
+	local function toboolean( value ) -- helper for animate fields
+		-- units will be animated upon leveling or killing, even
+		-- with special attacker and defender values
+		if value then return true
+		else return false end
+	end
+
+	local this_unit <close> = utils.scoped_var("this_unit")
+
+	for index, unit_to_harm in ipairs(wesnoth.units.find_on_map(filter)) do
+		if unit_to_harm.valid then
+			-- block to support $this_unit
+			wml.variables["this_unit"] = nil -- clearing this_unit
+			wml.variables["this_unit"] = unit_to_harm.__cfg -- cfg field needed
+			local amount = tonumber(cfg.amount)
+			local animate = cfg.animate -- attacker and defender are special values
+			local delay = cfg.delay or 500
+			local kill = cfg.kill
+			local fire_event = cfg.fire_event
+			local primary_attack = wml.get_child(cfg, "primary_attack")
+			local secondary_attack = wml.get_child(cfg, "secondary_attack")
+			local harmer_filter = wml.get_child(cfg, "filter_second")
+			local experience = cfg.experience
+			local resistance_multiplier = tonumber(cfg.resistance_multiplier) or 1
+			if harmer_filter then harmer = wesnoth.units.find_on_map(harmer_filter)[1] end
+			-- end of block to support $this_unit
+
+			if animate then
+				if animate ~= "defender" and harmer and harmer.valid then
+					wesnoth.interface.scroll_to_hex(harmer.x, harmer.y, true)
+					wml_actions.animate_unit {
+						flag = "attack",
+						hits = true,
+						with_bars = true,
+						T.filter { id = harmer.id },
+						T.primary_attack ( primary_attack ),
+						T.secondary_attack ( secondary_attack ),
+						T.facing { x = unit_to_harm.x, y = unit_to_harm.y },
+					}
+				end
+				wesnoth.interface.scroll_to_hex(unit_to_harm.x, unit_to_harm.y, true)
+			end
+
+			-- the two functions below are taken straight from the C++ engine,
+			-- utils.cpp and actions.cpp, with a few unuseful parts removed
+			-- may be moved in helper.lua in 1.11
+			local function round_damage( base_damage, bonus, divisor )
+				local rounding
+				if base_damage == 0 then return 0
+				else
+					if bonus < divisor or divisor == 1 then
+						rounding = divisor / 2 - 0
+					else
+						rounding = divisor / 2 - 1
+					end
+					return math.max( 1, math.floor( ( base_damage * bonus + rounding ) / divisor ) )
+				end
+			end
+
+			local function calculate_damage( base_damage, alignment, tod_bonus, resistance, modifier )
+				local damage_multiplier = 100
+				if alignment == "lawful" then
+					damage_multiplier = damage_multiplier + tod_bonus
+				elseif alignment == "chaotic" then
+					damage_multiplier = damage_multiplier - tod_bonus
+				elseif alignment == "liminal" then
+					damage_multiplier = damage_multiplier + math.max(0, wesnoth.current.schedule.liminal_bonus - math.abs(tod_bonus))
+				else -- neutral, do nothing
+				end
+				local resistance_modified = resistance * modifier
+				damage_multiplier = damage_multiplier * resistance_modified
+				local damage = round_damage( base_damage, damage_multiplier, 10000 ) -- if harmer.status.slowed, this may be 20000 ?
+				return damage
+			end
+
+			local damage = calculate_damage(
+				amount,
+				cfg.alignment or "neutral",
+				wesnoth.schedule.get_illumination(unit_to_harm).lawful_bonus,
+				100 - unit_to_harm:resistance_against( cfg.damage_type or "dummy" ),
+				resistance_multiplier
+			)
+
+			if unit_to_harm.hitpoints <= damage then
+				if kill == false then damage = unit_to_harm.hitpoints - 1
+				else damage = unit_to_harm.hitpoints
+				end
+			end
+
+			unit_to_harm.hitpoints = unit_to_harm.hitpoints - damage
+			local text = string.format("%d%s", damage, "\n")
+			local add_tab = false
+			local gender = unit_to_harm.__cfg.gender
+
+			local function set_status(name, male_string, female_string, sound)
+				if not cfg[name] or unit_to_harm.status[name] then return end
+				if gender == "female" then
+					text = string.format("%s%s%s", text, tostring(female_string), "\n")
+				else
+					text = string.format("%s%s%s", text, tostring(male_string), "\n")
+				end
+
+				unit_to_harm.status[name] = true
+				add_tab = true
+
+				if animate and sound then -- for unhealable, that has no sound
+					wesnoth.audio.play(sound)
+				end
+			end
+
+			if not unit_to_harm.status.unpoisonable then
+				set_status("poisoned", _"poisoned", _"female^poisoned", "poison.ogg")
+			end
+			set_status("slowed", _"slowed", _"female^slowed", "slowed.wav")
+			set_status("petrified", _"petrified", _"female^petrified", "petrified.ogg")
+			set_status("unhealable", _"unhealable", _"female^unhealable")
+
+			-- Extract unit and put it back to update animation if status was changed
+			unit_to_harm:extract()
+			unit_to_harm:to_map()
+
+			if add_tab then
+				text = string.format("%s%s", "\t", text)
+			end
+
+			if animate and animate ~= "attacker" then
+				if harmer and harmer.valid then
+					wml_actions.animate_unit {
+						flag = "defend",
+						hits = true,
+						with_bars = true,
+						T.filter { id = unit_to_harm.id },
+						T.primary_attack ( primary_attack ),
+						T.secondary_attack ( secondary_attack ),
+						T.facing { x = harmer.x, y = harmer.y },
+					}
+				else
+					wml_actions.animate_unit {
+						flag = "defend",
+						hits = true,
+						with_bars = true,
+						T.filter { id = unit_to_harm.id },
+						T.primary_attack ( primary_attack ),
+						T.secondary_attack ( secondary_attack ),
+					}
+				end
+			end
+
+			wesnoth.interface.float_label( unit_to_harm.x, unit_to_harm.y, string.format( "<span foreground='red'>%s</span>", text ) )
+
+			local function calc_xp( level ) -- to calculate the experience in case of kill
+				if level == 0 then return math.ceil(wesnoth.game_config.kill_experience / 2)
+				else return level * wesnoth.game_config.kill_experience end
+			end
+
+			if experience ~= false and harmer and harmer.valid
+				and wesnoth.sides.is_enemy( unit_to_harm.side, harmer.side )
+			then
+				if kill ~= false and unit_to_harm.hitpoints <= 0 then
+					harmer.experience = harmer.experience + calc_xp( unit_to_harm.level )
+				else
+					unit_to_harm.experience = unit_to_harm.experience + harmer.level
+					harmer.experience = harmer.experience + wesnoth.game_config.combat_experience * unit_to_harm.level
+				end
+			end
+
+			if kill ~= false and unit_to_harm.hitpoints <= 0 then
+				wml_actions.spartan_kill { id = unit_to_harm.id, animate = toboolean( animate ), fire_event = fire_event, harmer and T.secondary_unit { id = harmer.id } }
+			end
+
+			if animate then
+				wesnoth.interface.delay(delay)
+			end
+
+			if variable then
+				wml.variables[string.format("%s[%d]", variable, index - 1)] = { harm_amount = damage }
+			end
+
+			-- both units may no longer be alive at this point, so double check
+			if experience ~= false and unit_to_harm and unit_to_harm.valid then
+				unit_to_harm:advance(toboolean(animate), fire_event ~= false)
+			end
+
+			if experience ~= false and harmer and harmer.valid then
+				harmer:advance(toboolean(animate), fire_event ~= false)
+			end
+		end
+
+		wml_actions.redraw {}
+	end
+end
---
+++ lua/spartan_kill.lua
@@ -0,0 +1,134 @@
+--copy of the [kill] tag lua code (1.16 version, might need to update sometimes), that respects revives
+
+local helper = wesnoth.require "helper"
+local location_set = wesnoth.require "location_set"
+
+local kill_recursion_preventer = location_set.create()
+
+debug_utils = wesnoth.require "~add-ons/1The_Great_Steppe_Era/lua/debug_utils.lua"
+
+function wesnoth.wml_actions.spartan_kill(cfg)
+	local number_killed = 0
+	local secondary_unit = wml.get_child(cfg, "secondary_unit")
+	local killer_loc = {0, 0}
+	if secondary_unit then
+		secondary_unit = wesnoth.units.find_on_map(secondary_unit)[1]
+		if cfg.fire_event then
+			if secondary_unit then
+				killer_loc = { x = tonumber(secondary_unit.x) or 0, y = tonumber(secondary_unit.y) or 0 }
+			else
+				wesnoth.log("warn", "failed to match [secondary_unit] in [kill] with a single on-board unit")
+			end
+		end
+	end
+	local dead_men_walking = wesnoth.units.find_on_map(cfg)
+	for i,unit in ipairs(dead_men_walking) do
+		local death_loc = {x = tonumber(unit.x) or 0, y = tonumber(unit.y) or 0}
+		if not secondary_unit then killer_loc = death_loc end
+		local can_fire = false
+
+		local recursion = (kill_recursion_preventer:get(death_loc.x, death_loc.y) or 0) + 1
+		if cfg.fire_event then
+			kill_recursion_preventer:insert(death_loc.x, death_loc.y, recursion)
+			can_fire = true
+			if death_loc.x == wesnoth.current.event_context.x1 and death_loc.y == wesnoth.current.event_context.y1 then
+				if wesnoth.current.event_context.name == "die" or wesnoth.current.event_context.name == "last breath" then
+					if recursion >= 10 then
+						can_fire = false;
+						wesnoth.log("error", "tried to fire 'die' or 'last breath' event "
+							.. "on unit from the unit's 'die' or 'last breath' event "
+							.. "with first_time_only=no!")
+					end
+				end
+			end
+		end
+		if can_fire then
+			wesnoth.fire_event("last breath", death_loc, killer_loc)
+		end
+
+--SPARTAN EDITED CODE HERE:
+		local unit_test_if_still_alive = wesnoth.units.find_on_map({ id = unit.id})
+
+		local unit_test_hp = 0
+
+		--if there is a unit still left, set hp var
+
+		if #unit_test_if_still_alive > 0 then
+			unit_test_hp = unit_test_if_still_alive[1].hitpoints
+		end
+
+		if cfg.animate and unit.valid == "map" and unit_test_hp < 1 then
+			wesnoth.interface.scroll_to_hex(death_loc, true)
+			local anim = wesnoth.units.create_animator()
+			local primary = wml.get_child(cfg, "primary_attack")
+			local secondary = wml.get_child(cfg, "secondary_attack")
+			-- Yes, we get the primary attack from the secondary unit and vice versa
+			-- The primary attack in a death animation is the weapon that caused the death
+			-- In other words, the attacker's weapon. The attacker is the secondary unit.
+			-- In the victory animation, this is simply swapped.
+			if primary then
+				if secondary_unit then
+					primary = secondary_unit:find_attack(primary)
+				else
+					primary = wesnoth.units.create_weapon(primary)
+				end
+				wesnoth.log('err', "Primary weapon:\n" .. wml.tostring(primary.__cfg))
+			end
+			if secondary then
+				if primary then
+					secondary = unit:find_attack(secondary)
+				else
+					secondary = wesnoth.units.create_weapon(secondary)
+				end
+				wesnoth.log('err', "Secondary weapon:\n" .. wml.tostring(secondary.__cfg))
+			end
+			anim:add(unit, "death", "kill", {primary = primary, secondary = secondary})
+			if secondary_unit then
+				anim:add(secondary_unit, "victory", "kill", {primary = secondary, secondary = primary})
+			end
+			anim:run()
+		end
+		-- wesnoth.wml_actions.redraw{}
+
+
+--SPARTAN EDITED CODE added hp check:
+		if can_fire and unit_test_hp < 1  then
+			wesnoth.fire_event("die", death_loc, killer_loc)
+		end
+		if cfg.fire_event then
+			if recursion <= 1 then
+				kill_recursion_preventer:remove(death_loc.x, death_loc.y)
+			else
+				kill_recursion_preventer:insert(death_loc.x, death_loc.y, recursion)
+			end
+		end
+		-- Test that it's valid (and still on the map) first, in case the event erased (or extracted) it.
+
+
+--SPARTAN EDITED CODE PART 2 HERE:
+
+		unit_test_if_still_alive = wesnoth.units.find_on_map({ id = unit.id})
+
+		if #unit_test_if_still_alive > 0 then
+			unit_test_hp = unit_test_if_still_alive[1].hitpoints
+		end
+
+--		debug_utils.dbms(unit_test_if_still_alive, true, "killed unit", true)
+
+		if unit.valid == "map" and unit_test_hp < 1 then unit:erase() end
+
+		-- wesnoth.wml_actions.redraw{}
+
+		number_killed = number_killed + 1
+	end
+
+	if (cfg.x == "recall" or cfg.x == nil) and (cfg.y == "recall" or cfg.y == nil) then
+		local dead_men_sleeping = wesnoth.units.find_on_recall(cfg)
+		for i,unit in ipairs(dead_men_sleeping) do
+			unit:erase()
+		end
+		number_killed = number_killed + #dead_men_sleeping
+	end
+
+	return number_killed
+end
