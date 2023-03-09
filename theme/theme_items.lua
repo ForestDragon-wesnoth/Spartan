3ceaf8a533eb5a1e268927f387d593ef7c006f1f
ForestDragon <kolya.vlasenko@gmail.com>
2023-03-09 13:57:02 +0200

    working on custom UI again

--- theme/theme_items.lua
+++ theme/theme_items.lua
@@ -1,43 +1,4 @@
-local colors = { ---blue = args.ge_blue ,
-		 numbers = '#f5e6c1' ,
-		 detail_dark = '#a69275' ,
-		 red = '#fa8c50',
-		 gray = '#808080' }
-
-
-local function production(side, resource)
-	local var_name = resource .. "_factory"
-	local len = wml.variables[var_name .. ".length"]
-	local prod = 0
-	for i=0,(len - 1) do
-		local factory = wml.variables[var_name .. "[".. i .."]"]
-		if wesnoth.map.get_owner(factory) == side then
-			prod = prod + factory[resource]
-		end
-	end
-	return prod
-end
-
-function wesnoth.interface.game_display.goldincome()
-	-- Display for the viewing side, not the current side
-	local viewing_side = wesnoth.interface.get_viewing_side()
-
-	local side_proxy = wesnoth.sides[viewing_side]
-	local val = side_proxy.gold or 0
-	local income = side_proxy.net_income or 0
-	local sign = "+"
-	if income < 0 then
-		sign = ""
-	end
-	local str = val .. " (" .. sign .. income .. ")"
-
-	return { { 'element', {
-		text = str,
-		tooltip = "<b>Gold</b>: "..val.."\n<b>Income</b>: ".. sign .. income .. "\nWith gold you buy new units and pay upkeep for units on the board. To increase your income, capture villages."
-
-	} } }
-
-end
+local _ = wesnoth.textdomain "wesnoth-Hoplite"
 
 function wesnoth.interface.game_display.killstreak()
 	-- Display for the viewing side, not the current side
@@ -52,23 +13,27 @@ function wesnoth.interface.game_display.killstreak()
 
 	return { { 'element', {
 		text = str,
-		tooltip = "<b>Killstreak</b>: \nAmount of units killed in a row. Important for some upgrades, as well as achievements like Frenzy"
+		tooltip = _"<span color='#ff5050'><b>Killstreak</b></span>: \nAmount of units killed in a row. Important for some upgrades, as well as achievements like Frenzy"
 
 	} } }
 
 end
 
-function wesnoth.interface.game_display.coal()
+function wesnoth.interface.game_display.orbs()
 	-- Display for the viewing side, not the current side
 	local viewing_side = wesnoth.interface.get_viewing_side()
 
-	local val = wml["variables"]["coal_reserve" .. viewing_side] or 0
-	local prod = production (viewing_side, "coal")
-	local str = val .. " (+" .. prod .. ")"
+	local side_proxy = wesnoth.sides[viewing_side]
+	local val = wml.variables["spartan_orbs_of_insight"..wesnoth.interface.get_viewing_side()] or 0
+	local str = val
+	--if (viewing_side ~= wesnoth.current.side) then
+	--	str = "<span color='" .. colors.gray .. "'>" .. str .. "</span>"
+	--end
 
 	return { { 'element', {
 		text = str,
-		tooltip = "<b>Coal</b>: "..val.."\n<b>Coal production</b>: +" .. prod .. "\nCoal is needed for machines to be able to move. To get more coal, capture coal factories"
+		tooltip = "<span color='#a456ff'><b>Orbs of Insight</b></span>: \nThese mysterious orbs can be used to reveal hints for hidden achievements, or spent as currency in a certain rare location. Both the hints and the orb itself carry over between playthroughs."
+
 	} } }
 
 end
